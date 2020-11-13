48627ed05f8a56be656541efcebc775afe776b09
Adnan Raja <58033249+adnannrajaa@users.noreply.github.com>
2020-11-14 02:11:26 +0500

    Update

---
+++ ChatView/wwwroot/js/chat.js
@@ -0,0 +1,95 @@
+﻿"use strict";
+const BaseUrl = "http://localhost:57127/";
+$(() => {
+    GetUserDetails()
+})
+
+//Disable buttons until connection is established
+document.getElementById("sendButton").disabled = true;
+document.getElementById("sendGroupButton").disabled = true;
+document.getElementById("groupButton").disabled = true;
+
+let access_token = localStorage.getItem("UserToken");
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl(BaseUrl + "chatHub",
+        {
+            accessTokenFactory: async () => {
+                return access_token;
+            }
+        })
+    .build();
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    document.getElementById("sendGroupButton").disabled = false;
+    document.getElementById("groupButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.on("Send", function (message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    //var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendGroupButton").addEventListener("click", function (event) {
+    var groupName = document.getElementById("groupInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendGroupMessage", groupName, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("groupButton").addEventListener("click", function (event) {
+    var roomName = document.getElementById("groupInput").value;
+    connection.invoke("AddToGroup", roomName).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+let GetUserDetails = () => {
+    let user = null;
+    user = parseJwt(localStorage.getItem("UserToken"));
+    if (!user) {
+        $("#UserDetails").text("UnAuthorized")
+    } else {
+        var str = `User Id = ${user.user_id}, User Name = ${user.user_name}`
+        $("#UserDetails").text(str)
+    }
+}
+
+function parseJwt(token) {
+    if (token == null || token == 'null') {
+
+        return false;
+    }
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
---
+++ ChatView/wwwroot/js/users.js
@@ -0,0 +1,75 @@
+﻿const BaseUrl = "http://localhost:57127/";
+$(() => {
+    loadAllUsers();
+})
+let loadAllUsers =()=> {
+    $.ajax({
+        url: BaseUrl + "api/UserAccount",
+        type: "GET",
+        timeout: 0,
+        async: false,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', "Bearer " + localStorage.getItem("UserToken"));
+        },
+        success: function (response) {
+            if (response.length > 0) {
+                mapUserData(response)
+            }
+        }
+
+    });
+}
+
+let clearUserList = () => {
+    $("#UserList").empty()
+}
+let mapUserData = (arg_data) => {
+    clearUserList()
+    arg_data.map((row, index) => {
+        var str = '';
+        str = `<li>${index + 1} : ${row.user_name}</li>`
+        $("#UserList").append(str);
+    })
+
+}
+$("#loadButton").on("click", () => {
+    loadAllUsers();
+})
+$("#sendButton").on("click", () => {
+    var obj = {
+        user_name: $("#userInput").val(),
+        password: $("#passwordInput").val()
+    }
+    $.ajax({
+        url: BaseUrl + "api/UserAccount",
+        type: "Post",
+        contentType: 'application/json; charset=utf-8',
+        dataType : "json",
+        data: JSON.stringify(obj),
+        success: function (response) {
+            alert("User Added Successfully.");
+        }
+
+    });
+})
+
+$("#loginButton").on("click", () => {
+    var obj = {
+        user_name: $("#userInput").val(),
+        password: $("#passwordInput").val()
+    }
+    $.ajax({
+        url: BaseUrl + "api/UserAccount/authenticate",
+        type: "Post",
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(obj),
+        success: function (response) {
+            console.log(response)
+            if (response.token != null) {
+                localStorage.setItem("UserToken", response.token)
+                alert("User Login Successfully.");
+            }
+        }
+
+    });
+})
